bc893db10a00f74084aaed088892273e4104376c
MobiLab Interns <mobilabinterns@MobiLabs-MacBook-Pro.local>
2024-03-19 12:01:35 +0100

    Added new error and CreatedAt and DeadLine

--- src/main/kotlin/com/SumiProject/first/web/ExeptionControllerAdvice.kt
+++ src/main/kotlin/com/SumiProject/first/web/ExeptionControllerAdvice.kt
@@ -1,11 +1,13 @@
 package com.SumiProject.first.web
 
+import com.SumiProject.first.errors.BadRequestError
 import com.SumiProject.first.errors.NotFoundError
 import org.apache.logging.log4j.message.Message
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.client.HttpClientErrorException.BadRequest
 
 data class ErrorResponse(
     val status: Int,
@@ -36,6 +38,15 @@ class ExceptionControllerAdvice {
             ex.message
         )
         return ResponseEntity(errorMessage, HttpStatus.NOT_FOUND)
+    }
+    @ExceptionHandler
+    fun handleBadRequestError(ex: BadRequestError): ResponseEntity<ErrorResponse> {
+
+        val errorMessage = ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            ex.message
+        )
+        return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
 
 }
