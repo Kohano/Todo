29c0064d5f88b3cc84118be78a22e9beb07db297
MobiLab Interns <mobilabinterns@MobiLabs-MacBook-Pro.local>
2024-03-20 15:04:19 +0100

    Finished

--- src/main/kotlin/com/SumiProject/first/web/TodoController.kt
+++ src/main/kotlin/com/SumiProject/first/web/TodoController.kt
@@ -1,62 +1,37 @@
 package com.sumiProject.first.web
 
-import com.SumiProject.first.Domain.Todo
-import com.SumiProject.first.Domain.UserService
-import com.SumiProject.first.errors.BadRequestError
-import com.SumiProject.first.errors.NotFoundError
+import com.SumiProject.first.Domain.TodoService
 import com.SumiProject.first.web.CreateTodoRequest
-import com.SumiProject.first.web.ErrorResponse
 import com.SumiProject.first.web.TodoResponse
 import com.SumiProject.first.web.UpdateTodoRequest
-import org.apache.coyote.BadRequestException
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.client.HttpClientErrorException.BadRequest
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import kotlin.random.Random
-
 
 @RestController
 class TodoController (
-    val userService: UserService
+  val todoService: TodoService
 ){
-
     @GetMapping("/todos")
     fun getTodos(): List<TodoResponse> {
         println("Hit the getUsers() endpoint")
-        return  userService.getTodos().map {
+        return  todoService.getTodos().map {
             TodoResponse(it.id, it.title, it.status, it.createdAt, it.deadLine)
         }
     }
 
-
-
-
     @GetMapping("/todos/{todoId}")
     fun getTodos(@PathVariable todoId: Int): TodoResponse{
-        val todo = userService.getTodoById(todoId)
+        val todo = todoService.getTodoById(todoId)
         return TodoResponse(todo.id, todo.title, todo.status, todo.createdAt, todo.deadLine)
     }
 
     @PostMapping("/todos")
     fun createTodo(
         @RequestBody request: CreateTodoRequest
-    ): Todo {
+    ): TodoResponse {
+        println("Hit the createUsers() endpoint")
         println("Received request $request")
-        val newTodoId: Int = Random.nextInt(0, 10000)
-        val todo = Todo(
-            id = newTodoId,
-            title = request.title,
-            deadLine = request.deadLine
-        )
-        if (request.deadLine != null && request.deadLine!! < LocalDateTime.now()){
-            throw BadRequestError("User can not set deadLine on Past. ")
-        }
-
-
-        todoList.add(todo)
-        return todo
+        val createdTodo = todoService.createTodo(request.title,request.deadLine)
+        return TodoResponse(createdTodo.id,createdTodo.title,createdTodo.status,createdTodo.createdAt,createdTodo.deadLine )
     }
 
     @PatchMapping("/todos/{todoId}")
@@ -66,47 +41,21 @@ class TodoController (
 
         @RequestBody
        update: UpdateTodoRequest
-   ): Todo{
+   ): TodoResponse {
         println("Received request ${update}")
-        var existingTodo: Todo? = null
-        for(todo in todoList) {
-            if (todo.id == todoId) {
-                existingTodo = todo
-            }
-        }
+        val updateTodo = todoService.updateTodo(todoId,update.title,update.deadLine,update.status  )
+        return TodoResponse(updateTodo.id,updateTodo.title,updateTodo.status,updateTodo.createdAt,updateTodo.deadLine)
+    }
 
-        if (existingTodo == null){
-            throw NotFoundError("User with id $todoId not found!")
-        }
-        println(existingTodo)
-         if (update.deadLine != null && update.deadLine!! < LocalDateTime.now()){
-             throw BadRequestError("User can not set deadLine on Past. ")
-         }
-         existingTodo.status = update.status
-         existingTodo.deadLine = update.deadLine
-         existingTodo.title = update.title
-        return existingTodo
-   }
     @DeleteMapping("/todos/{todoId}")
-    fun delteTodo(
+    fun deleteTodo(
         @PathVariable
-        todoId: Int
-    ): Todo{
-        var existingTodo: Todo? = null
-        for(todo in todoList) {
-            if (todo.id == todoId) {
-                existingTodo = todo
-            }
-
+        todoId: Int,
+    ): TodoResponse{
+        val deleteTodo=todoService.deleteTodo(todoId)
+        return TodoResponse(deleteTodo.id,deleteTodo.title,deleteTodo.status,deleteTodo.createdAt,deleteTodo.deadLine)
         }
-        if (existingTodo == null){
-           throw NotFoundError("User with id $todoId not found!")
-        }
-        todoList.remove(existingTodo)
-
-        return existingTodo
     }
 
 
 
-}
--- src/main/kotlin/com/SumiProject/first/web/TodoDtos.kt
+++ src/main/kotlin/com/SumiProject/first/web/TodoDtos.kt
@@ -7,15 +7,17 @@ data class CreateTodoRequest(
     val title: String,
     var deadLine: LocalDateTime?
 )
+
 data class UpdateTodoRequest(
     val title: String,
     val status: Status,
     var deadLine: LocalDateTime?
 )
+
 data class TodoResponse(
     val id : Int,
     var title : String,
-    var status: Status = Status.InProgress,
-    val createdAt: LocalDateTime = LocalDateTime.now(),
-    var deadLine : LocalDateTime? = null
+    var status: Status,
+    val createdAt: LocalDateTime,
+    var deadLine : LocalDateTime?
 )
